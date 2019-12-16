f2cf49f3193beeb8c647207843247585cc406630
Victor Peralta <victorperalta.correo@gmail.com>
2019-12-16 23:30:28 +0100

    Examenes completados

---
+++ examenes/examen22.m
@@ -0,0 +1,43 @@
+% EJEMPLO 2
+%% Ejercicio 1
+x = 0:255; c= 255 / log(256);
+y = c*log(x+1);
+I = imread('radio.tif');
+I_log = intlut(I,uint8(y));
+figure, subplot(1,3,1), plot(y), axis tight, axis square
+subplot(1,3,2), imshow(I), title('Imagen original')
+subplot(1,3,3), imshow(I_log), title('Imagen transformada')
+
+%% Ejercicio 2
+I = imread('eight.tif');
+figure, subplot(2,2,1), imshow(I), title('Imagen original')
+tf = cumsum(imhist(I));
+tf_norm = tf / max(tf);
+subplot(2,2,2), plot(tf_norm), axis tight
+
+%% Ejercicio 3
+a = [0 0 0 1 0 0 0]
+f = [1 2 3 4 5]
+g = imfilter(a,f,'full','corr')
+% same indica que el vector de salida sea del mismo tamaño que el de
+% entrada
+% full indica que la salida sea el vector filtrado completo, por tanto
+% mayor en tamaño que la entrada
+
+%% Ejercicio 4
+fn_gau = fspecial('gaussian',9,1.5);
+figure, bar3(fn_gau,'b'), title('Filtro Gaussiano como gráfico 3D');
+I = imread('cameraman.tif');
+I_new3 = imfilter(I,fn_gau);
+figure
+subplot(1,2,1), imshow(I), title('Imagen original');
+subplot(1,2,2), imshow(I_new3), title('Filtro Gaussiano');
+
+%% Ejercicio 5
+D0 = 35; n = 3;
+H_but = 1 ./ (1 + (dist ./ D0) .^ (2 * n));
+figure, imshow(Id), title('Imagen Original');
+figure, imshow(log(1 + abs(fftshift(I_dft))),[]),
+title('FT de imagen original'); figure, mesh(fftshift(dist)),
+title('Matriz de distancias'); figure, imshow(fftshift(H_but)),
+title('Paso Bajo Butterworth');
