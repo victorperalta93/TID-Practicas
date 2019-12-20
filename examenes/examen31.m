b0f0770e50fdb9b2d6f00dcf29c86ed1e838eb49
Victor Peralta <victorperalta.correo@gmail.com>
2019-12-20 01:18:19 +0100

    Ejercicio 2 de examen 3.1 terminado

--- examenes/examen31.m
+++ examenes/examen31.m
@@ -18,3 +18,30 @@ I_blur2 = imfilter(I,f2);
 subplot(2,2,4), imshow(I_blur2),title('Promedio con tamaño núcleo 5x5');
 
 %% Ejercicio 2
+I = imread('cameraman.tif');
+I = im2double(I);imshow(I);
+[M, N] = size(I);
+
+% Generamos un filtro de emborronamiento de tipo gaussiano
+m=7; n=7;
+h = fspecial('gaussian',[m n],3);
+
+% Realizamos la convolución en el dominio espacial
+Ib = imfilter(I,h,'circular','same');
+
+% Reubicamos el filtro para calcular su transformada de Fourier
+hext = padarray(h,[M-m N-n],0,'post');
+horigen = circshift(hext, [-(m-1)/2 -(n-1)/2]);
+
+% Realizamos ahora la convolución en el dominio de Fourier
+fftIb = fft2(I).*fft2(horigen);
+
+% Llevamos la convolución al dominio espacial
+IbporFourier = real(ifft2(fftIb));
+
+% Comprobamos que la convolución en el dominio espacial y cuando usamos
+% transformada de Fourier coinciden y mostramos la imagen borrosa
+max(abs(Ib(:)-IbporFourier(:)))
+figure; imshow(Ib)
+
+%% Ejercicio 3
